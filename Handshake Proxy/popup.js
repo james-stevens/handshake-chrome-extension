e21c6fec6c12b03795b3d6c301fb696254240627
James Stevens <github@jrcs.net>
2022-03-02 17:16:22 +0000

    Initial

---
+++ Handshake Proxy/popup.js
@@ -0,0 +1,47 @@
+
+document.addEventListener("DOMContentLoaded", () => { startUp(); });
+
+function startUp()
+{
+	let tickbox = document.form1.handshake;
+	if (tickbox) tickbox.addEventListener("change", (event) => {
+		let tickbox = document.form1.handshake;
+		if (tickbox.checked) setProxyOn(); else setProxyOff();
+		});
+	getProxy();
+}
+
+
+function getProxy()
+{
+	chrome.proxy.settings.get( {'incognito': false},
+		(config) => { 
+			let tickbox = document.form1.handshake;
+			if (tickbox) 
+				tickbox.checked = (config.value.mode == "fixed_servers");
+			});
+}
+
+
+function setProxyOn()
+{
+	let config = {
+		mode: "fixed_servers",
+		rules: {
+			singleProxy: {
+				scheme: "http",
+				host: "109.169.23.69",
+				port: 3128
+				},
+			bypassList: ["<local>","*.co","*.ly","*.com","*.net","*.uk","*.us","*.de"]
+			}
+		};
+	chrome.proxy.settings.set({value: config, scope: "regular"}, () => { getProxy() });
+}
+
+
+function setProxyOff()
+{
+	let config = { mode: "system" };
+	chrome.proxy.settings.set({value: config, scope: "regular"}, () => { getProxy() });
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # handshake-chrome-extension
 Chrome Extension to give users access to Handshake HTTP sites
+
+NOTE: this sends your HTTP & HTTPS requests via an anonymous proxy - no logging
+but probably best to not leave it switched on when doing serious stuff.
